0b343a4951cb4ada594f45c52e9ba68c2a36f1ab
Peter Galer <pdgaler@gmail.com>
2023-01-30 20:40:18 -0500

    Trying to get into right format

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -24,16 +24,16 @@ class LinearRegression:
         """
         # Check if array has a determinant
         if np.linalg.det(X.T @ X) != 0:
-            X_train_append = np.hstack((X, np.ones((X.shape[0],1))))
+            X_train_append = np.hstack((X, np.ones((X.shape[0], 1))))
             wb = np.linalg.inv(X_train_append.T @ X_train_append) @ X_train_append.T @ y
-            
+
             self.w = wb[:-1]
             self.b = wb[-1]
 
         else:
             print("LinAlgError. Matrix is Singular. No analytical solution.")
 
-    def predict(self, X:np.ndarray) -> np.ndarray:
+    def predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predicts out out put using previously calculated weights using linear regression.
 
@@ -41,10 +41,11 @@ class LinearRegression:
             X (np.ndarray): Input data to predict.
         Returns:
             prediction (np.ndarray): Predicted values.
-        """        
+        """
         prediction = X @ self.w + self.b
 
         return prediction
+
 
 class GradientDescentLinearRegression(LinearRegression):
     """
@@ -52,33 +53,33 @@ class GradientDescentLinearRegression(LinearRegression):
     """
 
     def _squared_error(self, yhat, y):
-        '''
+        """
         Squared error loss function.
-        
+
         Args:
             y_hat (torch.tensor): Predicted values.
             y (torch.tensor): True values.
-        
+
         Returns:
             err (FLOAT): The squared error (loss).
-        
-        '''
+
+        """
         err = (yhat - y.reshape(yhat.shape)) ** 2
         return err
-    
+
     def _gradient_descent(self, w, b, lr):
-        '''
+        """
         Gradient_descent algorithm.
-        
+
         Args:
             w (torch.tensor): Model weights.
             b : torch.tensor: Bias.
             lr (FLOAT): learning rate.
-        
+
         Returns:
         w, b.
-        
-        '''
+
+        """
         with torch.no_grad():
             w -= w.grad * lr
             b -= b.grad * lr
@@ -104,7 +105,7 @@ class GradientDescentLinearRegression(LinearRegression):
         X_train = torch.tensor(X, dtype=torch.float32)
         y_train = torch.tensor(y.squeeze(), dtype=torch.float32)
 
-        w = torch.zeros(X.shape[1], 1 , dtype=torch.float32, requires_grad=True)
+        w = torch.zeros(X.shape[1], 1, dtype=torch.float32, requires_grad=True)
         b = torch.zeros(1, dtype=torch.float32, requires_grad=True)
 
         for e in range(epochs):
@@ -131,5 +132,5 @@ class GradientDescentLinearRegression(LinearRegression):
 
         prediction = torch.tensor(X, dtype=torch.float32) @ self.w + self.b
         pred_np = prediction.detach().numpy()
-        
-        return(pred_np)
+
+        return pred_np
