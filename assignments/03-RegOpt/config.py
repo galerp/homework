622fb4321c8de4963600c01bf5c6ceaa93089b0e
Peter Galer <pdgaler@gmail.com>
2023-02-18 23:04:48 -0500

    fixed?

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -11,14 +11,14 @@ class CONFIG:
     num_epochs = 15
     initial_learning_rate = 0.001
     initial_weight_decay = 0.01
-    lambda1 = lambda epoch: (1 - (epoch / 2)) ** 1.0
-    lr_lambda = [lambda1]
+    # lambda1 = lambda epoch: (1 - (epoch / 2)) ** 1.0
+    # lr_lambda = [lambda1]
 
     lrs_kwargs = {
         "initial_weight_decay": initial_weight_decay,
         "epochs": num_epochs,
         "batch_size": batch_size,
-        "lr_lambda_l": lr_lambda,
+        "lr_lambda_l": [lambda epoch: (1 - (epoch / 2)) ** 1.0],
         "initial_learning_rate": initial_learning_rate,
     }
 
