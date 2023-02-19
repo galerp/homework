6eab217e4100e03ae493f2817b3530ba2c7d68a1
Peter Galer <pdgaler@gmail.com>
2023-02-18 22:52:46 -0500

    config fix?

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,13 +8,10 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
 
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 15
     initial_learning_rate = 0.001
-    # initial_weight_decay = initial_learning_rate/num_epochs
     initial_weight_decay = 0.01
     lambda1 = lambda epoch: (1 - (epoch / 2)) ** 1.0
-    # lambda1 = lambda epoch: (1/(1+((0.01/16) * epoch)))
-
     lr_lambda = [lambda1]
 
     lrs_kwargs = {
