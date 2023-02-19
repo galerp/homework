6e976cfb2a7e11092283733eb09b05eaa39a80a5
Peter Galer <pdgaler@gmail.com>
2023-02-18 22:46:17 -0500

    test run

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -10,17 +10,27 @@ class CONFIG:
     batch_size = 64
     num_epochs = 2
     initial_learning_rate = 0.001
-    initial_weight_decay = 0
+    # initial_weight_decay = initial_learning_rate/num_epochs
+    initial_weight_decay = 0.01
+    lambda1 = lambda epoch: (1 - (epoch / 2)) ** 1.0
+    # lambda1 = lambda epoch: (1/(1+((0.01/16) * epoch)))
+
+    lr_lambda = [lambda1]
 
     lrs_kwargs = {
-        # You can pass arguments to the learning rate scheduler
-        # constructor here.
+        "initial_weight_decay": initial_weight_decay,
+        "epochs": num_epochs,
+        "batch_size": batch_size,
+        "lr_lambda_l": lr_lambda,
+        "initial_learning_rate": initial_learning_rate,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,9 @@
 from typing import List
-
 from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    def __init__(self, optimizer, last_epoch=-1, gamma=0.1, **kwargs):
         """
         Create a new scheduler.
 
@@ -12,13 +11,36 @@ class CustomLRScheduler(_LRScheduler):
         if you need to add new parameters.
 
         """
-        # ... Your Code Here ...
+        lr_lambda = kwargs.get("lr_lambda_l")
+        self.lr_lambda = lr_lambda
+        self.last_epoch = last_epoch
+        self.optimizer = optimizer
+        self._step_count = 0
+        self.lr_lambdas = lr_lambda[0]
+
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+
+    def step(self, epoch=None):
+        """Step counting for each epoch
+
+        Arguments:
+            epoch (integer): number of epochs. Defaults to None.
+        """
+        self._step_count += 1
 
     def get_lr(self) -> List[float]:
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
-        # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # if self.last_epoch not in self.milestones:
+        #     return [group['lr'] for group in self.optimizer.param_groups]
+        # return [group['lr'] * self.gamma ** self.milestones[self.last_epoch]
+        #         for group in self.optimizer.param_groups]
+        if self._step_count > 0:
+            return [
+                param_group["lr"] * self.lr_lambdas(self._step_count)
+                for param_group in self.optimizer.param_groups
+            ]
+        else:
+            return [param_group["lr"] for param_group in self.optimizer.param_groups]
