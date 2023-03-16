fcdb3d4fbc800c4aba3102b8b57c3c5f2bd3a428
Peter Galer <pdgaler@gmail.com>
2023-03-15 21:59:08 -0400

    first attempt

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,15 +2,18 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 8
-    num_epochs = 8
+    batch_size = 28
+    num_epochs = 2
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    transforms = Compose([ToTensor()])
+    # for nomalization parameters: https://stackoverflow.com/questions/69747119/pytorch-cifar10-images-are-not-normalized
+    transforms = Compose(
+        [ToTensor(), Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))]
+    )
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,41 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
+    """Convolution Neural Net Model"""
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        """Initialize the model.
+        Arguments:
+            num_channels (int): The number of channels in the input.
+            num_classes (int): The number of classes in the dataset.
+        Returns:
+            None
+        """
+
+        super(Model, self).__init__()
+
+        self.conv1 = nn.Conv2d(num_channels, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """Forward pass of the model.
+        Arguments:
+            x (torch.Tensor): The input tensor.
+        Returns:
+            output (torch.Tensor): The output of the model.
+        """
+
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        output = self.fc3(x)
+        return output
